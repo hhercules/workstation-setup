4c612a271a8e3d06fd39fedd03125f317f776457
Rami Kiriako <rmkiriako@gmail.com>
2016-08-10 21:26:43 -0700

    Reset setup.sh to a state where it just installs Homebrew

---
+++ install-functions.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+function install_homebrew() {
+    /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+}
+
--- setup.sh
+++ setup.sh
@@ -1,40 +1,8 @@
 #!/usr/bin/env bash
 
-# Fail immediately if any errors occur
-set -e
+source ./banner-function.sh
+source ./install-functions.sh
 
-echo "Caching password..."
-sudo -K
-sudo true;
+banner "Installing Homebrew"
+install_homebrew
 
-SETUP_TYPE=$1
-MY_DIR="$(dirname "$0")"
-
-clear
-
-echo
-echo "Setting up a '$SETUP_TYPE' machine..."
-
-source ${MY_DIR}/xcode-license.sh
-
-# Note: Homebrew needs to be set up first
-source ${MY_DIR}/homebrew.sh
-source ${MY_DIR}/configuration-osx.sh
-source ${MY_DIR}/configuration-bash.sh
-source ${MY_DIR}/git.sh
-source ${MY_DIR}/git-aliases.sh
-source ${MY_DIR}/java.sh
-source ${MY_DIR}/ruby.sh
-source ${MY_DIR}/node.sh
-source ${MY_DIR}/node-packages.sh
-source ${MY_DIR}/cloud.sh
-source ${MY_DIR}/applications-common.sh
-
-if [ "${SETUP_TYPE}" == "designer" ]
-then
-  source ${MY_DIR}/applications-designer.sh
-fi
-
-source ${MY_DIR}/configurations.sh
-
-source ${MY_DIR}/finished.sh
