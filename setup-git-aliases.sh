ef6eb096683d7883af9f0bd30ee97481e43e1222
Rami Kiriako <rmkiriako@gmail.com>
2016-08-10 23:40:58 -0700

    Add some more logging to setup-git-aliases.sh script

--- setup-git-aliases.sh
+++ setup-git-aliases.sh
@@ -4,7 +4,14 @@ source ./banner-function.sh
 
 banner "Setting up Git aliases"
 
+echo "Setting up 'git st'"
 git config --global alias.st status
+
+echo "Settign up 'git co'"
 git config --global alias.co checkout
+
+echo "Setting up 'git lola'"
 git config --global alias.lola "log --graph --decorate --oneline --all"
+
+banner "SETTING UP GIT ALIASES IS COMPLETE"
 
